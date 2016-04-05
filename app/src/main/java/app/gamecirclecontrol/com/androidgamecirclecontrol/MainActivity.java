916b672dfff3b151a0088dae26063735699e2808
geogie <geogie.ren@firstleap.cn>
2016-04-05 13:57:31 +0800

    commit version 1.0

--- app/src/main/java/app/gamecirclecontrol/com/androidgamecirclecontrol/MainActivity.java
+++ app/src/main/java/app/gamecirclecontrol/com/androidgamecirclecontrol/MainActivity.java
@@ -2,11 +2,18 @@ package app.gamecirclecontrol.com.androidgamecirclecontrol;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import app.gamecirclecontrol.com.androidgamecirclecontrol.customview.MySurfaceView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        setContentView(R.layout.activity_main);
+        setContentView(new MySurfaceView(this));
     }
 }
